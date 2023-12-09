2221e6c2df8350c5b2f9bc4a7dc6f8694bc951d9
Mogrima <rebel.rs@yandex.ru>
2023-12-09 18:58:45 +0500

    the number of lives and hitbox of enemies is displayed only in the mode debag

--- module/Enemies/Enemy.js
+++ module/Enemies/Enemy.js
@@ -28,8 +28,10 @@ export class Enemy {
         context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
 
         // отобразим у каждого врага его жизни
-        context.fillStyle = 'black';
-        context.font = '20px Helvetica';
-        context.fillText(this.lives, this.x, this.y - 5);
+        if(this.game.debug) {
+            context.fillStyle = 'white';
+            context.font = '20px Helvetica';
+            context.fillText(this.lives, this.x, this.y - 5);
+        }
     }
 }
