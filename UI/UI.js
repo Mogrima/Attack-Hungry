7d9fde762f81eb528bd3db56c1a569b95876da8b
Mogrima <rebel.rs@yandex.ru>
2023-12-09 18:44:08 +0500

    fonts added and new colors have been applied to the text

--- UI/UI.js
+++ UI/UI.js
@@ -2,8 +2,11 @@ export class UI {
     constructor(game) {
         this.game = game;
         this.fontSize = 25;
-        this.fontFamily = 'Helvetica';
+        this.fontFamily = 'Unica One';
+        this.fontFamilyST = 'Akatab';
         this.color = 'white';
+        this.textColor = '#00ffff';
+        this.indicatorColor = '#66ff00';
     }
 
     draw(context) {
@@ -24,19 +27,20 @@ export class UI {
             let message1;
             let message2;
             if (this.game.isWin()) {
-                message1 = 'Победа!';
-                message2 = 'Отличная работа!';
+                message1 = 'angry hungry!';
+                message2 = 'ⵜⵓⵜⵍⴰⵢⵜ ⵜⴰⴱⵏⴳⴰⵍⵉⵜ (বাংলা) ⵜⴻⵍⵍⴰ';
             } else {
-                message1 = 'Попробуй еще раз!';
-                message2 = 'В следующий раз все получится!';
+                message1 = 'go all out!';
+                message2 = ' ⵢⵓⵔⴰ ⵉⵣⵍⴰⵏ ⵉⵏⴰⵎⵓⵔⵏ ⵏ ⴱⵏⴳⵍⴰⴷⵉⵛ ⴷ ⵍⵀⵉⵏⴷ';
             }
+            context.fillStyle = this.textColor;
             context.font = '70px ' + this.fontFamily;
             context.fillText(message1, this.game.width * 0.5, this.game.height * 0.5 - 20);
-            context.font = '25px ' + this.fontFamily;
+            context.font = '25px ' + this.fontFamilyST;
             context.fillText(message2, this.game.width * 0.5, this.game.height * 0.5 + 20);
         }
 
-
+        context.fillStyle = this.indicatorColor;
         for (let i = 0; i < this.game.ammo; i++) {
             context.fillRect(5 * i + 20, 50, 3, 20);
         }
