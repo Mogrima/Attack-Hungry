021ac7db028fcdf4ee78168252745793b6cc77c8
Mogrima <rebel.rs@yandex.ru>
2024-03-13 06:24:46 +0500

    Code formatted.

--- module/Enemies/Enemy.js
+++ module/Enemies/Enemy.js
@@ -29,43 +29,43 @@ export class Enemy {
 
     update() {
         if (!this.free) {
-          
-        // Обновляем x-координату врага (уменьшаем ее на величину speedX)
-        this.y -= this.speedY;
-        // Помечаем врага как удаленного, если он полностью пересечет левую границу игрового поля
-        if (this.y > this.game.height ) this.reset();
+                
+            // Обновляем x-координату врага (уменьшаем ее на величину speedX)
+            this.y -= this.speedY;
+            // Помечаем врага как удаленного, если он полностью пересечет левую границу игрового поля
+            if (this.y > this.game.height ) this.reset();
 
-        if (this.game.checkCollision(this.game.player, this)) {
-            this.reset();
-            for(let i = 0; i < 10; i++) {
-                this.game.particles.push(new Particle(this.game,
-                    this.x + this.width * 0.5, this.y + this.height * 0.5));
-            }  
-        }
-
-        this.game.player.projectiles.forEach(projectile => {
-            if (this.game.checkCollision(projectile, this)) {
-                this.lives--;
-                this.game.particles.push(new Particle(this.game, this.x + this.width * 0.5,
-                    this.y + this.height * 0.5));
-                projectile.markedForDeletion = true;
-                if (this.lives <= 0) {        
-                    this.reset();
-                    for(let i = 0; i < this.countParticles; i++) {
-                        this.game.particles.push(new Particle(this.game, this.x + this.width * 0.5,
-                            this.y + this.height * 0.5));
-                    }          
-                    if (!this.game.gameOver) this.game.score += this.score;
-                    if (this.game.isWin()) this.game.gameOver = true;
-                }
+            if (this.game.checkCollision(this.game.player, this)) {
+                this.reset();
+                for(let i = 0; i < 10; i++) {
+                    this.game.particles.push(new Particle(this.game,
+                        this.x + this.width * 0.5, this.y + this.height * 0.5));
+                }  
             }
-        });
 
-        if(this.frameX < this.maxFrame) {
-            this.frameX++;
-        }  else {
-            this.frameX = 0;
-        }
+            this.game.player.projectiles.forEach(projectile => {
+                if (this.game.checkCollision(projectile, this)) {
+                    this.lives--;
+                    this.game.particles.push(new Particle(this.game, this.x + this.width * 0.5,
+                        this.y + this.height * 0.5));
+                    projectile.markedForDeletion = true;
+                    if (this.lives <= 0) {        
+                        this.reset();
+                        for(let i = 0; i < this.countParticles; i++) {
+                            this.game.particles.push(new Particle(this.game, this.x + this.width * 0.5,
+                                this.y + this.height * 0.5));
+                        }          
+                        if (!this.game.gameOver) this.game.score += this.score;
+                        if (this.game.isWin()) this.game.gameOver = true;
+                    }
+                }
+            });
+
+            if(this.frameX < this.maxFrame) {
+                this.frameX++;
+            }  else {
+                this.frameX = 0;
+            }
         }
     }
 
