246a1e438e4f245ee22234bcb587b240a2c85638
Mogrima <rebel.rs@yandex.ru>
2024-03-19 13:13:26 +0500

    The width and height of the canvas are set to the width and height of the screen.

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ window.addEventListener('load', function () {
     // canvas setup
     const canvas = this.document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 1500;
-    canvas.height = 500; 
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight; 
 
     const game = new Game(canvas, ctx);
     let lastTime = 0;
