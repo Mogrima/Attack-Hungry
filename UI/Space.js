0fea3f95270f4c44297c83d5e27d721e2258110b
Mogrima <rebel.rs@yandex.ru>
2024-03-20 13:14:50 +0500

    Nebulae objects are connected to the Space class.

--- UI/Space.js
+++ UI/Space.js
@@ -1,5 +1,6 @@
 import { Planet } from "./Planet.js";
 import { Glare } from "./Glare.js";
+import { Nebula } from "./Nebula.js";
 
 export class Space {
     constructor(game) {
@@ -8,16 +9,22 @@ export class Space {
         this.numberOfPlanets = 10;
         this.glares = [];
         this.numberOfGlares = 20;
+        this.nebulae = [];
+        this.numberOfNebulae = 5;
     }
 
     resize() {
         this.createObstacles();
         this.createGlares();
+        this.createNebulae();
         this.planets.forEach(planet => {
             planet.resize();
         });
         this.glares.forEach(glare => {
             glare.resize();
+        });
+        this.nebulae.forEach(nebula => {
+            nebula.resize();
         });
     }
 
@@ -28,11 +35,17 @@ export class Space {
         this.glares.forEach(glare => {
             glare.update();
         });
+        this.nebulae.forEach(nebula => {
+            nebula.update();
+        });
     }
 
     draw() {
         this.glares.forEach(glare => {
             glare.draw();
+        });
+        this.nebulae.forEach(nebula => {
+            nebula.draw();
         });
         this.planets.forEach(planet => {
             planet.draw();
@@ -51,6 +64,13 @@ export class Space {
         this.glares = [];
         for (let i = 0; i < this.numberOfGlares; i++) {
             this.glares.push(new Glare(this.game));
+        }
+    }
+
+    createNebulae() {
+        this.nebulae = [];
+        for (let i = 0; i < this.numberOfNebulae; i++) {
+            this.nebulae.push(new Nebula(this.game));
         }
     }
 }
