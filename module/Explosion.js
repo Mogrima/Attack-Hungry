2d33503d9481241701a09b8b4a631db7b67432c6
Mogrima <rebel.rs@yandex.ru>
2024-04-20 17:34:40 +0500

    Implemented explosion class.

---
+++ module/Explosion.js
@@ -0,0 +1,48 @@
+export class Explosion {
+    constructor(game) {
+        this.game = game;
+        this.x;
+        this.y;
+        this.image = document.getElementById('explosions');
+        this.frameX;
+        this.frameY;
+        this.maxFrame = 5;
+        this.spriteSize = 80;
+        this.size = this.spriteSize;
+        this.width = this.size;
+        this.height = this.size;
+        this.free = true;
+    }
+
+    start(x, y) {
+        this.free = false;
+        this.x = x;
+        this.y = y;
+        this.frameX = 0;
+        this.frameY = Math.floor(Math.random() * 4);
+    }
+
+    reset() {
+        this.free = true;
+    }
+
+    update() {
+        if (!this.free) {
+            // анимация будет прокручиваться вместе с движением игрового мира
+            if (this.game.spriteUpdate) {
+                this.frameX++;
+            }
+
+            if (this.frameX > this.maxFrame) this.reset();
+        }
+    }
+
+    draw() {
+        if (!this.free) {
+            this.game.ctx.drawImage(this.image,
+                this.spriteSize * this.frameX, this.spriteSize * this.frameY,
+                this.spriteSize, this.spriteSize, this.x, this.y,
+                this.size, this.size);
+        }
+    }
+}
