13c2fe30aa5bf0920ab0cd3e88171c164fa61c77
Mogrima <rebel.rs@yandex.ru>
2024-04-22 14:33:37 +0500

    The main font depends on the aspect ratio of the game. All indents of game text and hunger and rage scales are multiplied by the game coefficient.

--- UI/UI.js
+++ UI/UI.js
@@ -1,7 +1,7 @@
 export class UI {
     constructor(game) {
         this.game = game;
-        this.fontSize = 25;
+        this.fontSize = 25 * this.game.ratio;
         this.largeFont = Math.ceil(90 * this.game.ratio);
         this.smallFont = Math.ceil(45 * this.game.ratio);
         this.fontFamily = 'Unica One';
@@ -21,11 +21,11 @@ export class UI {
         this.game.ctx.shadowColor = 'black';
         this.game.ctx.font = this.fontSize + 'px ' + this.fontFamily;
         // очки
-        this.game.ctx.fillText('Score: ' + this.game.score, 20, 40);
+        this.game.ctx.fillText('Score: ' + this.game.score, 20 * this.game.ratio, 40 * this.game.ratio);
         //
-        this.game.ctx.fillText('Hungry: ', 20, 100);
+        this.game.ctx.fillText('Hungry: ', 20 * this.game.ratio, 100 * this.game.ratio);
         //
-        this.game.ctx.fillText('Rage: ', 20, 160);
+        this.game.ctx.fillText('Rage: ' + this.game.rage, 20 * this.game.ratio, 160 * this.game.ratio);
          // таймер
          const formattedTime = (this.game.gameTime * 0.001).toFixed(1);
          this.game.ctx.fillText('Timer: ' + formattedTime, 20, 240);
@@ -52,15 +52,24 @@ export class UI {
 
         this.game.ctx.fillStyle = this.indicatorColor;
         for (let i = 0; i < this.game.ammo; i++) {
-            this.game.ctx.fillRect(5 * i + 20, 50, 3, 20);
+            this.game.ctx.fillRect((5 * i + 20)  * this.game.ratio,
+            50 * this.game.ratio,
+            3 * this.game.ratio,
+            20 * this.game.ratio);
         }
         this.game.ctx.fillStyle = this.hungryColor;
         for (let i = 0; i < this.game.hungry; i++) {
-            this.game.ctx.fillRect(5 * i + 20, 110, 3, 20);
+            this.game.ctx.fillRect((5 * i + 20) * this.game.ratio,
+            110 * this.game.ratio,
+            3 * this.game.ratio,
+            20 * this.game.ratio);
         }
         this.game.ctx.fillStyle = this.rageColor;
         for (let i = 0; i < this.game.rage; i++) {
-            this.game.ctx.fillRect(5 * i + 20, 170, 3, 20);
+            this.game.ctx.fillRect((5 * i + 20) * this.game.ratio,
+            170 * this.game.ratio,
+            3 * this.game.ratio,
+            20 * this.game.ratio);
         }
         this.game.ctx.restore();
     }
