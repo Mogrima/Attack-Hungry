76735b4f6eb4d77fcfd8c57582c4bdfe6be4a462
Mogrima <rebel.rs@yandex.ru>
2024-04-26 23:51:49 +0500

    A class of asteroids has been implemented that move in a straight line and do not allow the player to recharge.

---
+++ module/Enemies/Asteroid.js
@@ -0,0 +1,36 @@
+import {Enemy} from './Enemy.js';
+
+export class Asteroid extends Enemy {
+    constructor(game) {
+        super(game);
+        this.spriteWidth = 80;
+        this.spriteHeight = 80;
+        this.width;
+        this.height;
+        this.image = document.getElementById('asteroid');
+
+        this.lives = 1;
+        this.maxLives = this.lives;
+        this.score = this.lives;
+
+        this.maxFrame = 0;
+        this.countParticles = 0;
+        this.sizeModifier = 1;
+    }
+
+    start() {
+        super.start();
+        this.frameY = Math.floor(Math.random() * 4);
+        this.speedX = 0;
+        this.speedY = -2;
+        this.lives = this.maxLives;
+    }
+
+    update() {
+        super.update();
+        if (this.lives < 1) {
+            const explosion = this.game.getExplosion();
+            if (explosion) explosion.start(this.x + this.width * 0.5, this.y + this.height * 0.5);
+        }
+    }
+}
