f2093d14ea4498d0293b2fbcec38800c6926e470
Mogrima <rebel.rs@yandex.ru>
2024-04-30 20:25:42 +0500

    Changed font sizes and indentations for game text to make them larger, because gaming ratio depends on the height of the screen.

--- UI/UI.js
+++ UI/UI.js
@@ -1,9 +1,9 @@
 export class UI {
     constructor(game) {
         this.game = game;
-        this.fontSize = 25 * this.game.ratio;
-        this.largeFont = Math.ceil(90 * this.game.ratio);
-        this.smallFont = Math.ceil(45 * this.game.ratio);
+        this.fontSize = 50 * this.game.ratio;
+        this.largeFont = Math.ceil(180 * this.game.ratio);
+        this.smallFont = Math.ceil(90 * this.game.ratio);
         this.fontFamily = 'Unica One';
         this.fontFamilyST = 'Akatab';
         this.color = 'white';
@@ -21,14 +21,14 @@ export class UI {
         this.game.ctx.shadowColor = 'black';
         this.game.ctx.font = this.fontSize + 'px ' + this.fontFamily;
         // очки
-        this.game.ctx.fillText('Score: ' + this.game.score, 20 * this.game.ratio, 40 * this.game.ratio);
+        this.game.ctx.fillText('Score: ' + this.game.score, 40 * this.game.ratio, 80 * this.game.ratio);
         //
-        this.game.ctx.fillText('Hungry: ', 20 * this.game.ratio, 100 * this.game.ratio);
+        this.game.ctx.fillText('Hungry: ', 40 * this.game.ratio, 200 * this.game.ratio);
         //
-        this.game.ctx.fillText('Rage: ' + this.game.rage, 20 * this.game.ratio, 160 * this.game.ratio);
+        this.game.ctx.fillText('Rage: ' + this.game.rage, 40 * this.game.ratio, 320 * this.game.ratio);
          // таймер
          const formattedTime = (this.game.gameTime * 0.001).toFixed(1);
-         this.game.ctx.fillText('Timer: ' + formattedTime, this.game.width - 130 * this.game.ratio, 40 * this.game.ratio);
+         this.game.ctx.fillText('Timer: ' + formattedTime, this.game.width - 280 * this.game.ratio, 80 * this.game.ratio);
         // сообщения о победе или проигрыше
         if (this.game.gameOver) {
             this.game.ctx.textAlign = 'center';
@@ -45,34 +45,34 @@ export class UI {
             this.game.ctx.fillStyle = this.textColor;
             this.game.ctx.font = this.largeFont + 'px ' + this.fontFamily;
             this.game.ctx.fillText(message1, this.game.width * 0.5,
-                this.game.height * 0.5 - (30 * this.game.ratio));
+                this.game.height * 0.5 - (60 * this.game.ratio));
             this.game.ctx.font = this.smallFont + 'px ' + this.fontFamilyST;
             this.game.ctx.fillText(message2, this.game.width * 0.5,
-                this.game.height * 0.5 + (30 * this.game.ratio));
+                this.game.height * 0.5 + (60 * this.game.ratio));
             this.game.ctx.fillText(message3, this.game.width * 0.5,
-                this.game.height * 0.5 + (90 * this.game.ratio));
+                this.game.height * 0.5 + (180 * this.game.ratio));
         }
 
         this.game.ctx.fillStyle = this.indicatorColor;
         for (let i = 0; i < this.game.ammo; i++) {
-            this.game.ctx.fillRect((5 * i + 20)  * this.game.ratio,
-            50 * this.game.ratio,
+            this.game.ctx.fillRect((5 * i + 40)  * this.game.ratio,
+            100 * this.game.ratio,
             3 * this.game.ratio,
-            20 * this.game.ratio);
+            40 * this.game.ratio);
         }
         this.game.ctx.fillStyle = this.hungryColor;
         for (let i = 0; i < this.game.hungry; i++) {
-            this.game.ctx.fillRect((5 * i + 20) * this.game.ratio,
-            110 * this.game.ratio,
+            this.game.ctx.fillRect((5 * i + 40) * this.game.ratio,
+            220 * this.game.ratio,
             3 * this.game.ratio,
-            20 * this.game.ratio);
+            40 * this.game.ratio);
         }
         this.game.ctx.fillStyle = this.rageColor;
         for (let i = 0; i < this.game.rage; i++) {
-            this.game.ctx.fillRect((5 * i + 20) * this.game.ratio,
-            170 * this.game.ratio,
+            this.game.ctx.fillRect((5 * i + 40) * this.game.ratio,
+            340 * this.game.ratio,
             3 * this.game.ratio,
-            20 * this.game.ratio);
+            40 * this.game.ratio);
         }
         this.game.ctx.restore();
     }
