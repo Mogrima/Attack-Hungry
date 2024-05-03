8eeb4b447cac343877d2df8f5e227d73c536331d
Mogrima <rebel.rs@yandex.ru>
2024-05-03 21:47:32 +0500

    The numerical value of the rage scale has been removed.

--- UI/UI.js
+++ UI/UI.js
@@ -27,7 +27,7 @@ export class UI {
         //
         this.game.ctx.fillText('Hungry: ', 40 * this.game.ratio, 200 * this.game.ratio);
         //
-        this.game.ctx.fillText('Rage: ' + this.game.rage, 40 * this.game.ratio, 320 * this.game.ratio);
+        this.game.ctx.fillText('Rage: ', 40 * this.game.ratio, 320 * this.game.ratio);
          // таймер
          const formattedTime = (this.game.gameTime * 0.001).toFixed(1);
          this.game.ctx.fillText('Timer: ' + formattedTime, this.game.width - 280 * this.game.ratio, 80 * this.game.ratio);
