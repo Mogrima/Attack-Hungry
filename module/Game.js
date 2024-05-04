e2cdd44355caf763a1a277d7bda47895466c4715
Mogrima <rebel.rs@yandex.ru>
2024-05-04 18:03:40 +0500

    Added method to check device type.

--- module/Game.js
+++ module/Game.js
@@ -242,6 +242,17 @@ export class Game {
         }
     }
 
+    getDeviceType() {
+        const userAgent = navigator.userAgent.toLowerCase();
+        const isMobile = /mobile|iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(userAgent);
+       
+        if (isMobile) {
+          return "mobile";
+        } else {
+          return "desktop";
+        }
+      }
+
     draw() {
         this.space.draw();
         this.ui.draw();
